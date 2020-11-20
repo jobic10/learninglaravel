5b30542fd30b7905b63915ef0b573b66d780ecf8
Owonubi Job Sunday <jobowonubi@gmail.com>
2020-11-20 19:49:45 +0100

    Controllers

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\FirstController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -20,3 +20,5 @@ Route::get('/', function () {
 Route::get('/about', function () {
     return view('about');
 });
+
+Route::get('/contact', [FirstController::class, 'index']);
