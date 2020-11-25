de754d7c4d3c13788a65f4287c5720c4acd671f8
Owonubi Job Sunday <jobowonubi@gmail.com>
2020-11-25 04:58:52 +0100

    Day 6 - Model Stub

--- README.md
+++ README.md
@@ -120,6 +120,7 @@ The Laravel framework is open-sourced software licensed under the [MIT license](
         - One to Many
     - Stub
         - Controller Stub
+        - Model Stub
 
 
 ### Stackoverflow to the rescue
--- app/Models/StubModel.php
+++ app/Models/StubModel.php
@@ -1,11 +1,11 @@
 <?php
 
-namespace {{ namespace }};
+namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class {{ class }} extends Model
+class StubModel extends Model
 {
     use HasFactory;
     public $timestamps = false;
