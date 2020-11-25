f752bd5a6ee0bf17a67994443b7263c9dfd2170a
Owonubi Job Sunday <jobowonubi@gmail.com>
2020-11-25 04:51:29 +0100

    Reverse lookup

--- app/Models/Elective.php
+++ app/Models/Elective.php
@@ -8,4 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class Elective extends Model
 {
     use HasFactory;
+    public function student()
+    {
+        return $this->belongsTo('App\Models\Student');
+    }
 }
